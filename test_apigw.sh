d5e36c61717a0f8bd035b0d4f4251817da0a6ae3
Ming Chen <mingchen@ming-s-macbook.lan>
2019-07-24 21:43:02 -0400

    update

--- test_apigw.sh
+++ test_apigw.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #echo "API key in customer header"
-curl -X POST -H "content-type: application/json, X-API-KEY: PNhuKQCO7a1cMsP3stp8EuhaxaNrhtK61UnFS51j" -d @$1 https://4gshff13u2.execute-api.us-east-1.amazonaws.com/Prod  | jq '.'
+curl -X POST -H "content-type: application/json, X-API-KEY: xxxx" -d @$1 https://4gshff13u2.execute-api.us-east-1.amazonaws.com/Prod  | jq '.'
