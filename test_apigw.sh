90ceca9bf356f3a2c5c4e8a20de406bab77bea6e
Ming Chen <mingchen@ming-s-macbook.lan>
2019-07-24 21:37:35 -0400

    updated api with headerwq

--- test_apigw.sh
+++ test_apigw.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo "case 1: no API key"
-curl -X POST -H "content-type: application/json" -d @$1 https://bprnd1eb50.execute-api.us-east-1.amazonaws.com/Prod  | jq '.'
-#echo "case 2: API key in customer header"
-#curl -H "X-API-KEY: 80DuE6p3EY8pqZNQrxVQx3l9NlPryGhp12aNX7oA" -X POST --data-binary @$1 https://sy1uci2mx4.execute-api.us-east-1.amazonaws.com/beta |jq '.'
+#echo "API key in customer header"
+curl -X POST -H "content-type: application/json, X-API-KEY: PNhuKQCO7a1cMsP3stp8EuhaxaNrhtK61UnFS51j" -d @$1 https://4gshff13u2.execute-api.us-east-1.amazonaws.com/Prod  | jq '.'
